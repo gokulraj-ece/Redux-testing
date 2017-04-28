4dbe9c0f016a8a45261e24c20dd599f0998264e8
gokulraj-ece <gokulraj@ece.ubc.ca>
2017-04-27 21:36:52 -0700

    pushing to github

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,26 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
+// to group together similar tests
+describe('App', () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(App);
+  })
+  // to test a single attribute of a target
+  // it('shows the correct text', () => {
+  //   // create an instance of App
+  //   const component = renderComponent(App);
+  //   // to make an assertion about a target
+  //   expect(component).to.contain('React simple starter');
+  // });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 });
